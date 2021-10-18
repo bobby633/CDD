19399b0ea2889d0fddbe0c69401ced091ca1d6f5
bobby633 <55462517+bobby633@users.noreply.github.com>
2021-10-18 09:05:22 +0100

    synchronized int - 400ns

---
+++ Lab2/Lab2b/IntegerObj.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * 
+ * @author bobby
+ *
+ */
+
+ class IntegerObj {
+    int value;
+    IntegerObj(int val) {
+        this.value = val;
+    }
+    /**
+     * 
+     * @return returns value increased
+     */
+     int inc(){
+        this.value++;
+        return this.value;
+    }
+}
+	 
+   
+
---
+++ Lab2/Lab2b/Task.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.Semaphore;
+
+/**
+ * 
+ * @author bobby
+ *
+ */
+public class Task implements Runnable {
+private String name;
+
+static Semaphore mutex = new Semaphore(4);
+    private IntegerObj total;
+    /**
+     * 
+     * @param task_1 creates a task that gets executed 
+     * @param total makes a total that gets shown at the end
+     */
+    public Task(String task_1, IntegerObj total) {
+        name=task_1;
+        this.total = total;
+    }
+   
+    public void run()
+    {
+        try
+        {	
+        	//mutex.acquireUninterruptibly();
+            for (int i = 0; i<500; i++)
+            {
+            	
+             
+                total.inc();
+               
+                if (i%100==0){
+                   Thread.sleep(100); 
+                }
+                
+            }
+            //mutex.release();
+            System.out.println(name+" complete");
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
